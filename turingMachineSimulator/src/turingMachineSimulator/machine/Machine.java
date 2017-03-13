6f9245f81a02ecebdd1a6351314906f6aef0b708
sergiosd <sduartecc@gmail.com>
2017-03-13 04:03:24 -0300

    first commit, first sketch from the project.

---
+++ turingMachineSimulator/src/turingMachineSimulator/machine/Machine.java
@@ -0,0 +1,19 @@
+package turingMachineSimulator.machine;
+
+import java.util.List;
+
+public class Machine {
+	
+	private List<State> states;
+	private State initialState;
+	private State acceptState;
+	private State rejectState;
+	private List<String> inputAlphabet;
+	private List<String> tapeAlphabet;
+	
+	
+	
+	
+	
+
+}
---
+++ turingMachineSimulator/src/turingMachineSimulator/machine/State.java
@@ -0,0 +1,10 @@
+package turingMachineSimulator.machine;
+
+import java.util.List;
+
+public class State {
+	
+	private String name;
+	private List<Transition> transitionFunction;
+
+}
